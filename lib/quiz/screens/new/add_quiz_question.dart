5e882578d24538d6e41c76a0f4da7f7086858104
mouafo <hari.randoll@gmail.com>
2024-10-31 14:38:59 +0100

    update add question quiz

--- lib/quiz/screens/new/add_quiz_question.dart
+++ lib/quiz/screens/new/add_quiz_question.dart
@@ -68,179 +68,178 @@ class _AddQuizQuestionScreenState extends State<AddQuizQuestionScreen>
 
   @override
   Widget build(BuildContext context) {
-    return BlocListener<QuizCubit, QuizState>(
-      listener: onEventReceived,
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('Question'),
-          leading: IconButton(
-            icon: const Icon(Icons.arrow_back),
-            onPressed: () => Navigator.of(context).pop(),
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Question'),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () => Navigator.of(context).pop(),
         ),
-        body: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              PopupMenuButton(
-                itemBuilder: (_) => [
-                  const PopupMenuItem(
-                    value: ImageSource.camera,
-                    child: ListTile(
-                      leading: Icon(Icons.camera_alt),
-                      minLeadingWidth: 24.0,
-                      title: Text('Prendre une photo'),
-                      dense: true,
-                    ),
-                  ),
-                  const PopupMenuItem(
-                    value: ImageSource.gallery,
-                    child: ListTile(
-                      leading: Icon(Icons.image),
-                      minLeadingWidth: 24.0,
-                      title: Text('Choisir une image'),
-                      dense: true,
-                    ),
-                  ),
-                ],
-                onSelected: (value) =>
-                    onPickImage(source: value, context: context),
-                child: Container(
-                  height: 200,
-                  width: double.infinity,
-                  color: Theme.of(context).colorScheme.tertiaryContainer,
-                  child: Center(
-                    child: image.isEmpty
-                        ? SvgPicture.asset(Assets.iconPictureAdd)
-                        : Image.file(File(image),
-                            width: double.infinity, fit: BoxFit.cover),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            PopupMenuButton(
+              itemBuilder: (_) => [
+                const PopupMenuItem(
+                  value: ImageSource.camera,
+                  child: ListTile(
+                    leading: Icon(Icons.camera_alt),
+                    minLeadingWidth: 24.0,
+                    title: Text('Prendre une photo'),
+                    dense: true,
                   ),
                 ),
+                const PopupMenuItem(
+                  value: ImageSource.gallery,
+                  child: ListTile(
+                    leading: Icon(Icons.image),
+                    minLeadingWidth: 24.0,
+                    title: Text('Choisir une image'),
+                    dense: true,
+                  ),
+                ),
+              ],
+              onSelected: (value) =>
+                  onPickImage(source: value, context: context),
+              child: Container(
+                height: 200,
+                width: double.infinity,
+                color: Theme.of(context).colorScheme.tertiaryContainer,
+                child: Center(
+                  child: image.isEmpty
+                      ? SvgPicture.asset(Assets.iconPictureAdd)
+                      : Image.file(File(image),
+                          width: double.infinity, fit: BoxFit.cover),
+                ),
               ),
-              Padding(
-                  padding:
-                      const EdgeInsets.only(left: 16.0, right: 16.0, top: 16.0),
-                  child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        const SizedBox(height: 8),
-                        const Text('Question'),
-                        const SizedBox(height: 8),
-                        TextFormField(
-                            style: Theme.of(context).textTheme.bodyMedium,
-                            controller: _questionController,
-                            decoration: InputDecoration(
-                              hintText: 'Pose une question',
-                              hintStyle: Theme.of(context)
-                                  .textTheme
-                                  .bodyMedium!
-                                  .copyWith(
-                                    color: Theme.of(context)
-                                        .colorScheme
-                                        .onSurfaceVariant,
-                                  ),
-                            ),
-                            maxLines: 4,
-                            keyboardType: TextInputType.text,
-                            textCapitalization: TextCapitalization.sentences,
-                            textInputAction: TextInputAction.done,
-                            maxLengthEnforcement: MaxLengthEnforcement.enforced,
-                            onTapOutside: (event) =>
-                                FocusScope.of(context).unfocus(),
-                            onEditingComplete: FocusScope.of(context).unfocus,
-                            validator: (value) => Validators.empty(value)),
-                        const SizedBox(height: 16),
-                        const Text('Propositions'),
-                      ])),
-              const Divider(),
-              const SizedBox(height: 8),
-              Padding(
-                padding: const EdgeInsets.only(left: 16.0, right: 16.0),
+            ),
+            Padding(
+                padding:
+                    const EdgeInsets.only(left: 16.0, right: 16.0, top: 16.0),
                 child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    ..._propositionControllers.map((controller) {
-                      return Padding(
-                        padding: const EdgeInsets.only(bottom: 12.0),
-                        child: Row(
-                          children: [
-                            Expanded(
-                              child: TextFormField(
-                                  style: Theme.of(context).textTheme.bodyMedium,
-                                  controller: controller,
-                                  decoration: InputDecoration(
-                                    hintText: 'Entre le texte',
-                                    hintStyle: Theme.of(context)
-                                        .textTheme
-                                        .bodyMedium!
-                                        .copyWith(
-                                          color: Theme.of(context)
-                                              .colorScheme
-                                              .onSurfaceVariant,
-                                        ),
-                                  ),
-                                  keyboardType: TextInputType.text,
-                                  textCapitalization:
-                                      TextCapitalization.sentences,
-                                  textInputAction: TextInputAction.done,
-                                  maxLengthEnforcement:
-                                      MaxLengthEnforcement.enforced,
-                                  onTapOutside: (event) =>
-                                      FocusScope.of(context).unfocus(),
-                                  onEditingComplete:
-                                      FocusScope.of(context).unfocus,
-                                  validator: (value) =>
-                                      Validators.empty(value)),
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      const SizedBox(height: 8),
+                      const Text('Question'),
+                      const SizedBox(height: 8),
+                      TextFormField(
+                          style: Theme.of(context).textTheme.bodyMedium,
+                          controller: _questionController,
+                          decoration: InputDecoration(
+                            hintText: 'Pose une question',
+                            hintStyle: Theme.of(context)
+                                .textTheme
+                                .bodyMedium!
+                                .copyWith(
+                                  color: Theme.of(context)
+                                      .colorScheme
+                                      .onSurfaceVariant,
+                                ),
+                          ),
+                          maxLines: 4,
+                          keyboardType: TextInputType.text,
+                          textCapitalization: TextCapitalization.sentences,
+                          textInputAction: TextInputAction.done,
+                          maxLengthEnforcement: MaxLengthEnforcement.enforced,
+                          onTapOutside: (event) =>
+                              FocusScope.of(context).unfocus(),
+                          onEditingComplete: FocusScope.of(context).unfocus,
+                          validator: (value) => Validators.empty(value)),
+                      const SizedBox(height: 16),
+                      const Text('Propositions'),
+                    ])),
+            const Divider(),
+            const SizedBox(height: 8),
+            Padding(
+              padding: const EdgeInsets.only(left: 16.0, right: 16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  ..._propositionControllers.map((controller) {
+                    return Padding(
+                      padding: const EdgeInsets.only(bottom: 12.0),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: TextFormField(
+                                style: Theme.of(context).textTheme.bodyMedium,
+                                controller: controller,
+                                decoration: InputDecoration(
+                                  hintText: 'Entre le texte',
+                                  hintStyle: Theme.of(context)
+                                      .textTheme
+                                      .bodyMedium!
+                                      .copyWith(
+                                        color: Theme.of(context)
+                                            .colorScheme
+                                            .onSurfaceVariant,
+                                      ),
+                                ),
+                                keyboardType: TextInputType.text,
+                                textCapitalization:
+                                    TextCapitalization.sentences,
+                                textInputAction: TextInputAction.done,
+                                maxLengthEnforcement:
+                                    MaxLengthEnforcement.enforced,
+                                onTapOutside: (event) =>
+                                    FocusScope.of(context).unfocus(),
+                                onEditingComplete:
+                                    FocusScope.of(context).unfocus,
+                                validator: (value) => Validators.empty(value)),
+                          ),
+                          const SizedBox(width: 8),
+                          GestureDetector(
+                            onTap: () => selectCorrectAnswer(
+                                _propositionControllers.indexOf(controller)),
+                            child: Icon(
+                              Icons.check_circle_outline,
+                              size: 24,
+                              color: _correctAnswerIndex ==
+                                      _propositionControllers
+                                          .indexOf(controller)
+                                  ? Theme.of(context).colorScheme.primary
+                                  : Theme.of(context)
+                                      .colorScheme
+                                      .onSurfaceVariant,
                             ),
-                            const SizedBox(width: 8),
-                            GestureDetector(
-                              onTap: () => selectCorrectAnswer(
-                                  _propositionControllers.indexOf(controller)),
-                              child: Icon(
-                                Icons.check_circle_outline,
-                                size: 24,
-                                color: _correctAnswerIndex ==
-                                        _propositionControllers
-                                            .indexOf(controller)
-                                    ? Theme.of(context).colorScheme.primary
-                                    : Theme.of(context)
-                                        .colorScheme
-                                        .onSurfaceVariant,
-                              ),
-                            ),
-                          ],
-                        ),
-                      );
-                    }),
-                    const SizedBox(height: 8),
-                    if (_propositionControllers.length < 4)
-                      GestureDetector(
-                        onTap: _addProposition,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                              'Ajouter',
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .bodyMedium!
-                                  .copyWith(
-                                    color: Theme.of(context)
-                                        .colorScheme
-                                        .onSurfaceVariant,
-                                  ),
-                            ),
-                            const Icon(Icons.keyboard_arrow_down_outlined),
-                          ],
-                        ),
+                          ),
+                        ],
                       ),
-                  ],
-                ),
+                    );
+                  }),
+                  const SizedBox(height: 8),
+                  if (_propositionControllers.length < 4)
+                    GestureDetector(
+                      onTap: _addProposition,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Ajouter',
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodyMedium!
+                                .copyWith(
+                                  color: Theme.of(context)
+                                      .colorScheme
+                                      .onSurfaceVariant,
+                                ),
+                          ),
+                          const Icon(Icons.keyboard_arrow_down_outlined),
+                        ],
+                      ),
+                    ),
+                ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
-        bottomNavigationBar: SafeArea(
+      ),
+      bottomNavigationBar: BlocListener<QuizCubit, QuizState>(
+        listener: onEventReceived,
+        child: SafeArea(
           minimum: const EdgeInsets.symmetric(horizontal: 32.0, vertical: 16.0),
           child: Column(
             mainAxisSize: MainAxisSize.min,
